118c50437b3e1dd525249809e2417a2101cbce84
Alan Mathiasen <alanemathiasen@gmail.com>
2023-12-06 20:12:19 -0300

    it works

--- scenes/character.gd
+++ scenes/character.gd
@@ -1,24 +1,27 @@
 extends CharacterBody2D
 
-enum HookStates {NONE, EXTEND, HOOKED}
+enum HookStates {NONE, EXTEND, HOOKED, JUST_RELEASED}
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -800.0
-const SWING_SPEED : float = 0.001 # Adjust the swing speed as needed
-
+const JUMP_VELOCITY = -500.0
+const SWING_SPEED : float = 0.01 # Adjust the swing speed as needed
 const HOOK_SPEED = 1000.0
-const HOOK_SWING_RADIUS : float = 1.0  # Adjust the swing radius as needed
-var swing_angle : float = 1
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var hook_state: HookStates = HookStates.NONE
-var prev_position
-var hook_position : Vector2 = Vector2.ZERO
-var hook_length = 0.0
-var hook_right_direction = Vector2(cos(deg_to_rad(315)), sin(deg_to_rad(315)))
-var hook_left_direction = Vector2(cos(deg_to_rad(225)), sin(deg_to_rad(225)))
 
-var is_first := true
+var swing_angle := 1.0
+@export var gravity := 2000
+var hook_state := HookStates.NONE
+var prev_position
+var hook_position := Vector2.ZERO
+var hook_length = 0.0
+var hook_right_direction = Vector2(1, -1)
+var hook_left_direction = Vector2(-1, -1)
+var last_swing_velocity := Vector2.ZERO
+var angular_acceleration = 0.0
+var angular_velocity = 0.0
+
+# TODO this is just testing, delete
+var angle = 0
+
 var last_looking_right = true
 
 @onready var hook := $"../Hook"
@@ -26,6 +29,7 @@ var last_looking_right = true
 
 func _ready():
 	prev_position = global_position
+
 
 
 func _physics_process(delta):
@@ -44,28 +48,44 @@ func handle_character_movement(delta):
 	# 	velocity.y += 200
 	# 	hook.global_position = hook_position
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = direction * SPEED
-		if direction > 0:
-			last_looking_right = true
-		elif direction < 0:
-			last_looking_right = false
+	if(is_on_floor()):
+		if direction != 0:
+			velocity.x = direction * SPEED
+			if direction > 0:
+				last_looking_right = true
+			elif direction < 0:
+				last_looking_right = false
+		else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			if direction != 0:
+				velocity.x = direction * SPEED / 2
+				if direction > 0:
+					last_looking_right = true
+				elif direction < 0:
+					last_looking_right = false 
+
+		
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-
 func handle_hook(delta):
 	if Input.is_action_just_released("hook"):
+		velocity = Vector2(last_swing_velocity.x, -last_swing_velocity.y)
+		hook_state = HookStates.NONE
 		hide_hook()
+
 	elif Input.is_action_just_pressed("hook") and hook_state == HookStates.NONE:
 		extend_hook()
+		velocity.y = 0
+
+	if hook_state == HookStates.NONE:
+		hide_hook()
+		
 	if(hook_state == HookStates.EXTEND or hook_state == HookStates.HOOKED):
 		move_hook(delta)
-	if Input.is_action_pressed("hook") and hook_state == HookStates.HOOKED:
-		pass
+
 	if hook.visible:
 		line.points[1] = to_local(hook.global_position)
 
@@ -81,45 +101,51 @@ func hide_hook():
 	line.hide()
 
 func move_hook(delta):
-	var distance_travelled = global_position.distance_to(prev_position)
-	print(distance_travelled)
 	if hook_state == HookStates.EXTEND:
 		if last_looking_right:
-			hook.global_position += hook_right_direction * HOOK_SPEED * delta
+			hook.position += hook_right_direction * HOOK_SPEED * delta 
 		else: 
-			hook.global_position += hook_left_direction * HOOK_SPEED * delta
-		# hook.global_position += last_looking_right ? hook_right_direction * HOOK_SPEED * delta : hook_left_direction * HOOK_SPEED * delta 
+			hook.position += hook_left_direction * HOOK_SPEED * delta
 	elif hook_state == HookStates.HOOKED:
 		swing(delta)
-	# 	print('hola')
-	# 	# Calculate the direction from the player to the hooked position
-	# 	var direction_to_hook = (hook_position - global_position).normalized()
-	# 	# Rotate the direction to create a swinging effect
-	# 	var rotated_direction = direction_to_hook.rotated(swing_angle)
-	# 	# Update the player's position based on the hooked position and swinging
-	# 	global_position += rotated_direction * SWING_SPEED * delta
-	# 	# Increment the swing angle for the next frame
-	# 	#swing_angle += SWING_SPEED * delta
 
 func _on_hook_body_entered(body):
-	if(body.get_collision_layer() == 2 and hook_state == HookStates.EXTEND and is_first):
-		print(is_first)
-		is_first = false
+	if(body.get_collision_layer() == 2 and hook_state == HookStates.EXTEND):
 		hook_state = HookStates.HOOKED
 		hook_position = hook.global_position
 		hook_length = global_position.distance_to(hook_position)
+		angular_velocity += velocity.length() * 0.001
 
 func _on_hook_body_exited(_body:Node2D):
-	print('g')
-	is_first = true
+	angle = 0
+	pass
 
 func swing(delta):
-	var direction_to_hook = (hook_position - global_position).normalized()
-	var rotated_direction
-	var acceleration = hook_length * delta * 100
+	
+	# last_swing_velocity = Vector2(0, 0)
+	if (angle == 0):
+		if last_looking_right:
+			angle	= deg_to_rad(315) 
+			angular_velocity += 1
+			
+		else: 
+			angle	= deg_to_rad(45)
+			angular_velocity += -1
+			
+
+	angle += angular_velocity * delta
+	global_position = hook.global_position + Vector2(hook_length*sin(angle), hook_length*cos(angle))
+	last_swing_velocity = Vector2(hook_length * angular_velocity * cos(angle), hook_length * angular_velocity * sin(angle))
 	if last_looking_right:
-		rotated_direction = direction_to_hook.rotated(deg_to_rad(-HOOK_SWING_RADIUS))
+		if(angle > deg_to_rad(450)):
+			hook_state = HookStates.NONE 
+			velocity = last_swing_velocity
+			
+
 	else:
-		rotated_direction = direction_to_hook.rotated(deg_to_rad(HOOK_SWING_RADIUS))
-	global_position = hook_position - rotated_direction * hook_length
-	# swing_angle += SWING_SPEED * delta  # Increment the swing angle
+		if(angle < deg_to_rad(-180)):
+			hook_state = HookStates.NONE
+			# velocity = last_swing_velocity
+			velocity = last_swing_velocity
+			
+
