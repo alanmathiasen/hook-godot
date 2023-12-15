73bfaacf40ec429d4a61f5245f6368cdeaf1abc4
Alan Mathiasen <alanemathiasen@gmail.com>
2023-12-14 21:13:45 -0300

    start new hook handling

--- scenes/Hook.gd
+++ scenes/Hook.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 enum HookStates {NONE, EXTEND, HOOKED, JUST_RELEASED}
 
+signal hook_state_changed(hook_state)
+
 const HOOK_SPEED = 100.0
 const HOOK_DIRECTION_RIGHT = Vector2(1, -1)
 const HOOK_DIRECTION_LEFT = Vector2(-1, -1)
@@ -9,25 +11,26 @@ const HOOK_DIRECTION_LEFT = Vector2(-1, -1)
 @onready var line := $Line2D
 @onready var character := get_parent()
 
-var hook_state := HookStates.NONE
+var state := HookStates.NONE
 var hook_position := Vector2.ZERO
 var hook_length := 0.0
 var last_swing_velocity := Vector2.ZERO
 
 func _ready():
-  connect('body_entered', self, _on_hook_body_entered)
-  line.points[0] = Vector2.ZERO
-  self.hide()
+    line.points[0] = Vector2.ZERO
+    self.hide()
 
 func _input(event):
   if event.is_action_pressed('hook'):
-    if hook_state == HookStates.EXTEND:
-      reset_hook()
+    hook_state_changed.emit(HookStates.EXTEND)
+    pass
+    if state == HookStates.EXTEND:
+        reset_hook()
     else:
-      hook_state = HookStates.EXTEND
+        state = HookStates.EXTEND
 
 func _physics_process(delta):
-  match hook_state:
+  match state:
     HookStates.EXTEND:
       extend_hook(delta)
 
@@ -37,7 +40,7 @@ func extend_hook(delta):
   line.show()
 
   # move hook
-  position += HOOK_DIRECTION_RIGHT * HOOK_SPEED * delta
+  global_position += HOOK_DIRECTION_RIGHT * HOOK_SPEED * delta
 
   # draw line
   line.points[1] = to_local(character.global_position)
@@ -45,8 +48,6 @@ func extend_hook(delta):
 func reset_hook():
   self.hide()
   line.hide()
-  hook_state = HookStates.NONE
+  state = HookStates.NONE
   global_position = character.global_position
 
-func _on_hook_body_entered:
-  pass
--- scenes/character.gd
+++ scenes/character.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+enum HookStates {NONE, EXTEND, HOOKED, JUST_RELEASED}
+
 var speed := 400
-var gravity := 300
+var gravity := 500
 var jump_force = 600
 # var last_looking_right = true
+var local_hook_state = HookStates.NONE
 
 func _physics_process(delta):
+	if $Hook.state == HookStates.EXTEND:
+		print('hola')
 	handle_movement(delta)
 	move_and_slide()
 
@@ -20,8 +25,6 @@ func handle_movement(delta):
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = -jump_force
 
-
-
 func _on_hook_body_entered(body):
 	if body.get_collision_layer() == 2:
 		print('gancho')
@@ -29,5 +32,11 @@ func _on_hook_body_entered(body):
 		# hook_position = hook.global_position
 		# hook_length = global_position.distance_to(hook_position)
 
+
+
+func _on_hook_hook_state_changed(hook_state:Variant):
+	local_hook_state = hook_state
+	print(hook_state)
+	pass # Replace with function body.
 	
 
