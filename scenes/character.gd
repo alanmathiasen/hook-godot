3acdaaf7b3eb233c2ff3b365b486c878b8133c88
Alan Mathiasen <alanemathiasen@gmail.com>
2023-12-18 20:39:34 -0300

    it doesnt work anymore

--- scenes/character.gd
+++ scenes/character.gd
@@ -20,7 +20,7 @@ var linear_velocity := 0.0
 var velocity_arrow = Vector2.ZERO
 
 func _physics_process(delta):
-	print(velocity)
+	# print(velocity)
 	handle_hook(delta)
 	handle_movement(delta)
 	move_and_slide()
@@ -45,34 +45,37 @@ func handle_hook(delta):
 	if(hook.state == HookStates.HOOKED):
 		add_angular_velocity(facing_direction * 0.1)
 		linear_velocity = angular_velocity * distance_to_hook
-		print('linear', linear_velocity)
 		process_velocity(delta)
 
 func _on_hook_body_entered(_body):
 	distance_to_hook = self.global_position.distance_to(hook.global_position)
-
 	if facing_direction == 1:
-		angle_to_hook = atan2(hook.global_position.y - self.global_position.y, hook.global_position.x - self.global_position.x)
-	else:
 		angle_to_hook = atan2(self.global_position.y - hook.global_position.y, self.global_position.x - hook.global_position.x)
-	
+	else:		
+		angle_to_hook = atan2(self.global_position.y - hook.global_position.y, self.global_position.x - hook.global_position.x)
+		
 
 func process_velocity(delta:float)->void:
 	angular_acceleration = ((-gravity*delta) / distance_to_hook) * sin(angle_to_hook)
 	angular_velocity += angular_acceleration
-	angular_velocity *= 0.95
-	# print(angular_velocity)
-	angle_to_hook += angular_velocity * delta
-	
-	self.global_position = hook.global_position + Vector2(distance_to_hook * sin(angle_to_hook), distance_to_hook * cos(angle_to_hook))
+	print(angular_velocity)
+	# angular_velocity *= 0.95
+	print('angular_velocity ',angular_velocity)
+	angle_to_hook += angular_velocity 
+	print('hook.global_position ', hook.global_position)
+	print('el resto ', Vector2(distance_to_hook * cos(angle_to_hook), distance_to_hook * sin(angle_to_hook)))
+	self.global_position = hook.global_position + Vector2(distance_to_hook * cos(angle_to_hook), distance_to_hook * sin(angle_to_hook))
 
 func add_angular_velocity(force:float)->void:
 	angular_velocity += force
 
 func _on_hook_just_released():
-	print('linear', linear_velocity)
-	velocity.x += linear_velocity * cos(angle_to_hook)
-	velocity.y -= linear_velocity * sin(angle_to_hook)
+	# velocity.x += linear_velocity * cos(angle_to_hook)
+	# velocity.y += linear_velocity * sin(angle_to_hook)
+
+
+	print(rad_to_deg(angle_to_hook), 'sine', linear_velocity,  sin(angle_to_hook))
+	angular_velocity = 0.0
 	print('velocity', velocity)
 
 func _draw():
