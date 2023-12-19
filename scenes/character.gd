a4277167176a80375f6ef9c050da17b8e6b6136a
Alan Mathiasen <alanemathiasen@gmail.com>
2023-12-19 20:09:00 -0300

    working again

--- scenes/character.gd
+++ scenes/character.gd
@@ -19,6 +19,7 @@ var linear_velocity := 0.0
 
 var velocity_arrow = Vector2.ZERO
 
+
 func _physics_process(delta):
 	# print(velocity)
 	handle_hook(delta)
@@ -36,7 +37,7 @@ func handle_movement(delta):
 		velocity.x = speed * direction
 		facing_direction = 1 if direction > 0 else -1
 	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		velocity.x = move_toward(velocity.x, 0, 13)
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		velocity.y = -jump_force
 
@@ -49,26 +50,35 @@ func handle_hook(delta):
 
 func _on_hook_body_entered(_body):
 	distance_to_hook = self.global_position.distance_to(hook.global_position)
+	print('character y ', self.global_position.y, ' hook y: ', hook.global_position.y)
+	print('character x ', self.global_position.x, ' hook x: ', hook.global_position.x)
+	print('y de hook a character', (self.global_position.y - hook.global_position.y))
+
+	print('x de hook a character', (self.global_position.x - hook.global_position.x))
 	if facing_direction == 1:
-		angle_to_hook = atan2(self.global_position.y - hook.global_position.y, self.global_position.x - hook.global_position.x)
+		angle_to_hook = hook.get_angle_to(self.global_position)#atan2(-(self.global_position.y - hook.global_position.y), self.global_position.x - hook.global_position.x)
+		print('direction to hook y: ', (self.global_position.y - hook.global_position.y))
+		print('direction to hook x: ', (self.global_position.x - hook.global_position.x))
 		print(rad_to_deg(angle_to_hook))
 	else:		
-		angle_to_hook = atan2(hook.global_position.y - self.global_position.y, hook.global_position.x - self.global_position.x)
+		angle_to_hook = self.get_angle_to(hook.global_position) #atan2((self.global_position.y - hook.global_position.y), self.global_position.x - hook.global_position.x)
+		print('direction to hook y: ', (self.global_position.y - hook.global_position.y))
+		print('direction to hook x: ', (self.global_position.x - hook.global_position.x))
 		print(rad_to_deg(angle_to_hook))
 		
 
 func process_velocity(delta:float)->void:
-	angular_acceleration = 1 * delta#((-gravity*delta) / distance_to_hook) * sin(angle_to_hook)
+	angular_acceleration = 0.1 * delta#((-gravity*delta) / distance_to_hook) * sin(angle_to_hook)
 	if facing_direction == 1:
 		angular_velocity += angular_acceleration
 	else:
 		angular_velocity -= angular_acceleration
 
-	# print(angular_velocity)
 	# angular_velocity *= 0.95
 	# print('angular_velocity ',angular_velocity)
 	# print('angle_to_hook ',rad_to_deg(angle_to_hook))
 	angle_to_hook += angular_velocity * 0.01
+	# print(rad_to_deg(angle_to_hook))
 	# print('hook.global_position ', hook.global_position)
 	# print('el resto ', Vector2(distance_to_hook * cos(angle_to_hook), distance_to_hook * sin(angle_to_hook)))
 	self.global_position = hook.global_position - Vector2(distance_to_hook * sin(angle_to_hook), distance_to_hook * cos(angle_to_hook))
@@ -79,16 +89,18 @@ func add_angular_velocity(force:float)->void:
 
 func _on_hook_just_released():
 	var release_speed = linear_velocity
-	velocity.x = -release_speed * cos(angle_to_hook)
+	velocity.x = -release_speed * cos(angle_to_hook) * 1
 	velocity.y = 0
 	velocity.y = release_speed * sin(angle_to_hook)
 	# velocity.x -= linear_velocity * cos(angle_to_hook)
 	# velocity.y += linear_velocity * sin(angle_to_hook)
+	# Adjust the y component of the velocity
+	var adjustment_factor = 1 / abs(velocity.y - 1) + 1
+	velocity.y /= adjustment_factor * 2
 
-
-	print(rad_to_deg(angle_to_hook), 'sine', linear_velocity,  sin(angle_to_hook))
+	# print(rad_to_deg(angle_to_hook), 'sine', linear_velocity,  sin(angle_to_hook))
 	angular_velocity = 0.0
-	print('velocity', velocity)
+	# print('velocity', velocity)
 
 func _draw():
 	draw_line(Vector2.ZERO, velocity, Color.GREEN, 2.0)
